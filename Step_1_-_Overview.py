7c4e082362792ce6215f4b7e464b05ae4bae02e7
poeshead32 <152335585+poeshead32@users.noreply.github.com>
2023-11-28 23:20:01 +0200

    Add files via upload

---
+++ Step_1_-_Overview.py
@@ -0,0 +1,105 @@
+import streamlit as st
+from openai import OpenAI
+
+# Initialize the progress value in session state if it doesn't exist
+if "progress" and "text" not in st.session_state:
+    st.session_state.progress = 0
+    st.session_state.text = 0
+
+# Increment the progress bar by a certain amount
+def increment_progress(amount):
+    st.session_state.progress += amount
+    st.session_state.text += amount
+
+# Display the progress bar
+st.progress(st.session_state.progress)
+#st.text(st.session_state.text)
+st.text(f"Home valuation {st.session_state.text}% complete")
+
+
+st.title("Step 1: Chat")
+api_key = "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"
+client = OpenAI(api_key=api_key)
+
+st.write(
+    """
+Welcome to this starter template for creating your own custom variant
+of ChatGPT.
+"""
+)
+
+st.info(
+    """
+**👨This is Harry The AI Home Evaluator**
+1. Talk to Harry and tell him about your home.
+2. He will compile the information and give you an AI powered valuation on your home.
+3. You'll receive an email with a comprehensive breakdown of your homes value.
+"""
+)
+
+prompt_template = """
+You are "Harry the Home Evaluator," a helpful virtual assistant dedicated to assessing and providing home value predictions for users. Your goal is to assist users in evaluating their homes and estimating their current market value.
+
+You'll guide users through a step-by-step process to gather essential information about their homes, and then you'll use a machine learning model to provide a home value prediction. Here's how the conversation should proceed:
+
+Introduction: Start by introducing yourself and explaining the process to the user.
+
+Basic Home Information: Ask the user to provide some basic details about their home, such as its age, size (in square meters), the number of bedrooms and bathrooms, and any unique features or amenities.
+
+Once the user has given you all the information, Instruct the user to go to Step 2 ('Go to Step 2' needs to be explicity stated by you for my code to read and go to next step): Renovations, in the side menu, where they can upload pictures and describe any renovations or upgrades they've made to the home. Encourage them to provide details about when the renovations were completed and the extent of the improvements.
+"""
+
+
+# When calling ChatGPT, we  need to send the entire chat history together
+# with the instructions. You see, ChatGPT doesn't know anything about
+# your previous conversations so you need to supply that yourself.
+# Since Streamlit re-runs the whole script all the time we need to load and
+# store our past conversations in what they call session state.
+
+prompt = st.session_state.get(
+    "prompt", [{"role": "system", "content": prompt_template}]
+)
+# Here we display all messages so far in our convo
+for message in prompt:
+    # If we have a message history, let's display it
+    if message["role"] != "system":
+        with st.chat_message(message["role"]):
+            st.write(message["content"])
+
+
+# This is where the user types a question
+question = st.chat_input("Ask me for instructions ")
+#check_for_openai_key()
+
+if question:  # Someone have asked a question
+    # First we add the question to our message history
+    prompt.append({"role": "user", "content": question})
+    
+
+    # Let's post our question and a place holder for the bot answer
+    with st.chat_message("user"):
+        st.write(question)
+
+    with st.chat_message("assistant"):
+        botmsg = st.empty()
+
+    # Here we call ChatGPT with streaming
+    response = []
+    result = ""
+    for chunk in client.chat.completions.create(
+        model="gpt-3.5-turbo", messages=prompt, stream=True
+    ):
+        text = chunk.choices[0].delta.content
+        if text is not None:
+            
+            response.append(text)
+            result = "".join(response).strip()
+            # Let us update the Bot's answer with the new chunk
+            botmsg.write(result)
+        
+    # When we get an answer back we add that to the message history
+    prompt.append({"role": "assistant", "content": result})
+    increment_progress(3)
+    # Finally, we store it in the session state
+    st.session_state["prompt"] = prompt
+
---
+++ pages/Step 2 - Renovations.py
@@ -0,0 +1,161 @@
+import streamlit as st
+import base64
+#import databutton as db
+
+from openai import OpenAI
+
+# Retrieve the chat history from the previous page
+previous_chat_history = st.session_state.get("prompt")
+
+# Function to encode the image to base64
+def encode_image(image_file):
+    return base64.b64encode(image_file.getvalue()).decode("utf-8")
+
+
+st.set_page_config(
+    page_title="Renovations", layout="centered", initial_sidebar_state="collapsed"
+)
+# Streamlit page setup
+st.title("Step 2: Renovations")
+
+
+# Retrieve the OpenAI API Key from secrets
+api_key = "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"
+
+# Initialize the OpenAI client with the API key
+client = OpenAI(api_key=api_key)
+
+# File uploader allows user to add their own image
+uploaded_file = st.file_uploader("Upload an image", type=["jpg", "png", "jpeg"])
+
+if uploaded_file:
+    # Display the uploaded image
+    with st.expander("Image", expanded=True):
+        st.image(uploaded_file, caption=uploaded_file.name, use_column_width=True)
+
+# Toggle for showing additional details input
+additional_details = st.text_area(
+    "Add any additional details or context about the image here:",
+)
+
+# Button to trigger the analysis
+analyze_button = st.button("Upload Images", type="secondary")
+
+# Check if an image has been uploaded, if the API key is available, and if the button has been pressed
+if uploaded_file is not None and api_key and analyze_button:
+    with st.spinner("Analysing the image ..."):
+        # Encode the image
+        base64_image = encode_image(uploaded_file)
+
+        # Create the prompt text with the previous chat history as context
+        prompt_text = "You are Harry the Home Evaluator, a helpful virtual assistant dedicated to assessing and providing home value predictions for users. Your goal is to assist users in evaluating their homes and estimating their current market value. "
+        if previous_chat_history:
+            prompt_text += "Previously, you had a conversation with the user:\n"
+
+            for message in previous_chat_history:
+                if message["role"] == "user":
+                    prompt_text += f"- User: {message['content']}\n"
+                elif message["role"] == "assistant":
+                    prompt_text += f"- Assistant: {message['content']}\n"
+
+        prompt_text += (
+            "You are now to receive images of any renovations the user has made, along with a description of those renovations. "
+            "You must comment on the renovations, make observations on what was done, and compliment the user for doing a good job."
+            "You must then instruct the user to go to the next page to receive their final valuation "
+        )
+        
+        if additional_details:
+            prompt_text += f"\n\nAdditional Context Provided by the User:\n{additional_details}"
+
+        # Create the payload for the completion request
+        messages = [
+            {
+                "role": "user",
+                "content": [
+                    {"type": "text", "text": prompt_text},
+                    {
+                        "type": "image_url",
+                        "image_url": f"data:image/jpeg;base64,{base64_image}",
+                    },
+                ],
+            }
+        ]
+
+        # Make the request to the OpenAI API
+        try:
+            # Stream the response
+            full_response = ""
+            message_placeholder = st.empty()
+            for completion in client.chat.completions.create(
+                model="gpt-4-vision-preview",
+                messages=messages,
+                max_tokens=1200,
+                stream=True,
+            ):
+                # Check if there is content to display
+                if completion.choices[0].delta.content is not None:
+                    full_response += completion.choices[0].delta.content
+                    message_placeholder.markdown(full_response + "▌")
+            # Final update to placeholder after the stream ends
+            message_placeholder.markdown(full_response)
+
+        except Exception as e:
+            st.error(f"An error occurred: {e}")
+
+else:
+    # Warnings for user action required
+    if not uploaded_file and additional_details and analyze_button:
+        prompt_text = "You are Harry the Home Evaluator, a helpful virtual assistant dedicated to assessing and providing home value predictions for users. Your goal is to assist users in evaluating their homes and estimating their current market value. "
+        if previous_chat_history:
+            prompt_text += "Previously, you had a conversation with the user:\n"
+
+            for message in previous_chat_history:
+                if message["role"] == "user":
+                    prompt_text += f"- User: {message['content']}\n"
+                elif message["role"] == "assistant":
+                    prompt_text += f"- Assistant: {message['content']}\n"
+
+        prompt_text += (
+            "You are now to recieve details on the customers renovations, but you were not uploaded an image. please recommend the customer upload an image for a more accurate valuation "
+            "You must comment on the renovations, make observations on what was done, and compliment the user for doing a good job."
+            "You must then instruct the user to go to the next page to receive their final valuation "
+        )
+        
+
+        prompt_text += f"\n\nAdditional Context Provided by the User:\n{additional_details}"
+
+        # Create the payload for the completion request
+        messages = [
+            {
+                "role": "user",
+                "content": [
+                    {"type": "text", "text": prompt_text},
+                    
+                ],
+            }
+        ]
+        #st.warning("Please upload an image.")
+        try:
+            # Stream the response
+            full_response = ""
+            message_placeholder = st.empty()
+            for completion in client.chat.completions.create(
+                model="gpt-4-vision-preview",
+                messages=messages,
+                max_tokens=1200,
+                stream=True,
+            ):
+                # Check if there is content to display
+                if completion.choices[0].delta.content is not None:
+                    full_response += completion.choices[0].delta.content
+                    message_placeholder.markdown(full_response + "▌")
+            # Final update to placeholder after the stream ends
+            message_placeholder.markdown(full_response)
+
+        except Exception as e:
+            st.error(f"An error occurred: {e}")
+
+    if not api_key:
+        st.warning("Please enter your OpenAI API key.")
+
+st.write(previous_chat_history)
